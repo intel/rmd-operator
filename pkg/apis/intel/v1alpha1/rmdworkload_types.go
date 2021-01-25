4300830e2aa66adb5fb91dd758d750f2fe569016
nolancon <conor.nolan@intel.com>
2021-01-25 16:27:20 +0000

    Add AllCores field to RmdWorkload

--- pkg/apis/intel/v1alpha1/rmdworkload_types.go
+++ pkg/apis/intel/v1alpha1/rmdworkload_types.go
@@ -49,11 +49,12 @@ type RmdWorkloadSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
-	CoreIds []string `json:"coreIds"`
-	Policy  string   `json:"policy,omniempty"`
-	Rdt     Rdt      `json:"rdt"`
-	Plugins Plugins  `json:"plugins,omniempty"`
-	Nodes   []string `json:"nodes"`
+	AllCores bool     `json:"allCores"`
+	CoreIds  []string `json:"coreIds"`
+	Policy   string   `json:"policy,omniempty"`
+	Rdt      Rdt      `json:"rdt"`
+	Plugins  Plugins  `json:"plugins,omniempty"`
+	Nodes    []string `json:"nodes"`
 }
 
 // RmdWorkloadStatus defines the observed state of RmdWorkload
